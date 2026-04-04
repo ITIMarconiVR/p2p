ff68feb317331616bf388efd910be1683e693254
campionl <luca.campion07@gmail.com>
2026-04-04 11:45:30 +0200

    fix: vedi eventi per admin e centralino

--- src/config.py
+++ src/config.py
@@ -45,6 +45,8 @@ def create_app():
     app.config['MAIL_PORT'] = 465
     app.config['MAIL_USE_TLS'] = False
     app.config['MAIL_USE_SSL'] = True
+    # app.config['MAIL_PORT'] = 587
+    # app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USERNAME'] = 'p2p@marconiverona.edu.it'
     app.config['MAIL_PASSWORD'] = 'jarc tihp ultm shhx' #'P2p2025$'
     app.config['MAIL_DEFAULT_SENDER'] = 'p2p@marconiverona.edu.it'
